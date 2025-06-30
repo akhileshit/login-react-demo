432bd8a30d373793ff44a517f93ae3c40967755f
akhileshit <akhil.11320@gmail.com>
2025-06-30 22:03:06 +0530

    First Commit

---
+++ src/Fail.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Fail() {
+  return (
+    <div>Login Failed</div>
+  )
+}
---
+++ src/Login_.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import Success from './Success'
+import Fail from './Fail'
+
+export default function Login() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [loginStatus, setLoginStatus] = useState(0)
+    
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (username === 'Ankit' && password === '123') {
+      setLoginStatus(1)
+      //we cant directly return the components from here (inside another funciton). only the Login() has the capability to return any component
+    } else {
+      setLoginStatus(2)
+    }
+  }
+
+  if (loginStatus === 1) {   // so written here outside another func, inside Login() 
+    return (
+      <Success/>
+    )
+  }
+  if (loginStatus === 2) {
+    return (
+      <Fail/>
+    )
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='username'>Username: </label>
+        <input type='text' id='username' onChange={(e) => setUsername(e.target.value)} />
+        
+        <label htmlFor='password'>Password: </label>
+        <input type='password' id='password' onChange={(e) => setPassword(e.target.value)}/>
+        
+        <button>Login</button>
+
+      </form>
+    </div>
+  )
+}
